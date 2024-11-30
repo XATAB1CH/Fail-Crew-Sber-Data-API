f362dc552b299ca9d9c57e768ca11a252f4286ea
mustafaevf <knetya274@gmail.com>
2024-11-30 21:31:52 +0300

    tw

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#F0F3F6",
+        primary: "#527AF2",
+        textColor: "#FFF",
+      }
+    },
   },
   plugins: [],
 }
