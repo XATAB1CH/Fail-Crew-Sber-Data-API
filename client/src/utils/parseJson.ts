6396a04a6be67cbbf11e10789924e42a1a0c1a06
mustafaevf <knetya274@gmail.com>
2024-11-30 21:32:04 +0300

    parseShemaJson

---
+++ client/src/utils/parseJson.ts
@@ -0,0 +1,41 @@
+
+const parseJson = (schema: any) => {
+    
+    if(!schema) {
+        return [];
+    }
+
+    const parseProperties = (properties: any, requiredFields: string[]) => {
+        return requiredFields.map((field: string) => {
+            if (properties[field]) {
+                return {
+                    fieldName: field,
+                    description: properties[field].description || "No desc",
+                    type: properties[field].type || "unknown",
+                };
+            } else {
+                return {
+                    fieldName: field,
+                    description: "Поле не найдено",
+                    type: "unknown",
+                };
+            }
+        });
+    };
+
+    const result: any[] = [];
+    schema.required.forEach((key: string) => {
+        const subSchema = schema.properties[key];
+        if (subSchema) {
+            const fields = parseProperties(subSchema.properties, subSchema.required || []);
+            result.push({
+                section: key,
+                fields,
+            });
+        }
+    });
+
+    return result;
+};
+
+export default parseJson;
